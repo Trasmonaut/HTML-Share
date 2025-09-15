144c09f3259fc340bf1957e476015b1c58b0cf81
Caleb <c.ramdath.26@gmail.com>
2025-09-15 01:47:44 -0400

    please be done

--- flippycards/flippycards.js
+++ flippycards/flippycards.js
@@ -13,7 +13,7 @@ async function downloadFile() {
     try {
 
         // example usage await addFileToZip('/neumorphism-404/neumorphism-404.html', 'neumorphism-404.html');
-        await addFileToZip('/flippycards.html', 'flippycards.html');
+        await addFileToZip('flippycards.html', 'flippycards.html');
         await addFileToZip('flippycards.css', 'flippycards.css');
         await addFileToZip('flippycards.js', 'flippycards.js');
 
